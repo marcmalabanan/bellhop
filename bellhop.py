87fd708a8d958cbb0e77850cdaff51e650c2ae9f
unknown <marc.malabanan@gmail.com>
2018-04-22 17:17:46 -0700

    Added /social, removed /imhere

--- bellhop.py
+++ bellhop.py
@@ -23,20 +23,20 @@ async def on_message(message):
             await message.channel.send("{0.author.mention}, <:ctd_jade:435997993171419136> I'm here <:ctd_jade:435997993171419136>".format(message))
 
 @bot.command()
-async def imhere(ctx):
-    await ctx.send("{0.author.mention}, <:ctd_jade:435997993171419136> I'm here <:ctd_jade:435997993171419136>".format(ctx))
-
-@bot.command()
 async def schedule(ctx):
     await ctx.send("{0.author.mention}, here is the current Back to Back podcast schedule: \n\n**Monday**: Black to Black with Amin / Black to Black overflow (Patreon exclusive) \n**Tuesday**: The Basketball Buds \n**Wednesday**: Zach Harper and Friends / SnarkHoops (Patreon exclusive) \n**Thursday**: Nerder She Wrote \n**Friday**: The Friday Mailbag \n\nOther Patron exclusive podcasts drop randomly!".format(ctx))
 
 @bot.command()
-async def wtf(ctx):
-    await ctx.send("https://cdn.vox-cdn.com/uploads/chorus_asset/file/9825985/2017_12_06_20_45_42.gif")
+async def social(ctx):
+    await ctx.send("{0.author.mention}, follow Count The Dings on social media: \n\n**Twitter**: http://www.twitter.com/countthedings \n**Facebook**: http://www.facebook.com/countthedings \n**YouTube**: http://bit.ly/2IqxSBC \n**Instagram**: http://www.instagram.com/countthedings".format(ctx))
 
 @bot.command()
 async def wow(ctx):
     await ctx.send("https://imgur.com/a/yPBWVcR")
+
+@bot.command()
+async def wtf(ctx):
+    await ctx.send("https://cdn.vox-cdn.com/uploads/chorus_asset/file/9825985/2017_12_06_20_45_42.gif")
 
 bot.remove_command('help')
 
@@ -44,11 +44,11 @@ bot.remove_command('help')
 async def help(ctx):
     embed = discord.Embed(title="Bellhop", description="The list of commands for the Bellhop are:", color=0xeee657)
 
-    embed.add_field(name="/imhere", value="Gives a special message from Jade.", inline=False)
-    embed.add_field(name="/schedule", value="Gives the current Back to Back podcast schedule.", inline=False)
-    embed.add_field(name="/wtf", value="Gives an appropriate 'wtf' gif.", inline=False)
-    embed.add_field(name="/wow", value="Gives an appropriate 'wow' gif.", inline=False)
     embed.add_field(name="/help", value="Gives this message.", inline=False)
+    embed.add_field(name="/schedule", value="Gives the current Back to Back podcast schedule.", inline=False)
+    embed.add_field(name="/social", value="Gives the social media links to Count The Dings.", inline=False)
+    embed.add_field(name="/wow", value="Gives an appropriate 'wow' gif.", inline=False)
+    embed.add_field(name="/wtf", value="Gives an appropriate 'wtf' gif.", inline=False)
 
     await ctx.send(embed=embed)
 
